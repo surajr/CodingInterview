b014f8718424500da28f1e67e6dc25ec81dd56f7
Suraj Rajasekhar <suraj.dec20@gmail.com>
2018-07-28 22:38:50 -0500

    07/28 containsNearbyDuplicate

--- java/containsNearbyDuplicate.java
+++ java/containsNearbyDuplicate.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        
         for(int i=0; i<nums.length; i++)
         {
-            if(map.containsKey(nums[i]))
-            {
-                if(i-map.get(nums[i]) <= k)
-                    return true;
-            }            
-                map.put(nums[i],i);
+            if(map.containsKey(nums[i]) && i-map.get(nums[i]) <= k)
+                return true;
+            map.put(nums[i],i);
         }
-        
         return false;
     }
 }
