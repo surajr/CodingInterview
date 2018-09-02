7c72e7539048f66330b60d54858d7404b00f9633
Suraj Rajasekhar <suraj.dec20@gmail.com>
2018-09-02 10:03:09 -0500

    1102

--- java/findDuplicates.java
+++ java/findDuplicates.java
@@ -1,3 +1,24 @@
+/*
+For every number at i, Flip the number. If the number is repeated next time and if number is less than 0,
+then it's a duplicate. Add this number to result.
+T - O(n)
+S - O(1)
+*/
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for(int i=0; i<nums.length; i++){
+            int index = Math.abs(nums[i])-1;
+            if(nums[index] < 0)
+                result.add(Math.abs(nums[i]));
+            else
+                nums[index] = -nums[index];
+        }
+        return result;
+    }
+}
+
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> ans=new ArrayList<>();
@@ -9,22 +30,6 @@ class Solution {
         for(int i=0 ; i<n ; i++){
             if(freq[i]==2){
                 ans.add(i+1);
-            }
-        }
-        return ans;
-    }
-}
-
-
-class Solution {
-    public List<Integer> findDuplicates(int[] nums) {
-        List<Integer> ans = new ArrayList<Integer>();
-        for(int i = 0; i < nums.length; i++){
-            int cur = Math.abs(nums[i]);
-            if(nums[cur-1]<0){
-                ans.add(cur);
-            }else{
-                nums[cur-1] = -1*nums[cur-1];
             }
         }
         return ans;
